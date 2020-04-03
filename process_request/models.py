7fb3f3faa3a27c2ef69b5bc52312760dcc354d6c
Patrick <pgalligan@rockarch.org>
2020-04-03 12:17:54 -0400

    add models
    adding models back

--- process_request/models.py
+++ process_request/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import (AbstractUser, AbstractBaseUser)
 
-# Create your models here.
+class User(AbstractUser):
+
+    archivist_group = [u"rac_archivists"]
+    donor_group = [u"rac_donors"]
+    researcher_group = [u"rac_researchers"]
+
+    AbstractUser._meta.get_field("email").blank = False
+    AbstractUser._meta.get_field("first_name").blank = False
+    AbstractUser._meta.get_field("last_name").blank = False
+    AbstractUser._meta.get_field("username").blank = False
+
+    @property
+    def full_name(self):
+        """
+        Return the first_name plus the last_name, with a space in between.
+        """
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def __str__(self):
+        """
+        Returns the full name and email of a user.
+        """
+        return '{} <{}>'.format(self.full_name, self.email)
+
+class MachineUser(AbstractBaseUser):
+    username = None
+    system_name = models.CharField(blank=False, max_length=255, unique=True)
+    host_location = models.URLField(blank=False)
+    api_key = models.CharField(blank=False, max_length=255)
+
+    is_active = models.BooleanField(
+        default=True,
+    )
+
+    USERNAME_FIELD = 'system_name'
+
+    def __str__(self):
+        """
+        Returns the system name.
+        """
+        return self.system_name
--- request_broker/settings.py
+++ request_broker/settings.py
@@ -99,6 +99,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'process_material.MachineUser'
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
