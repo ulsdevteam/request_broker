b288405bd98c1b5fc2a958fb1fe66b57d21b39a2
Patrick <pgalligan@rockarch.org>
2020-04-07 09:57:43 -0400

    add preliminary tests
    add preliminary tests for models

--- process_request/tests.py
+++ process_request/tests.py
@@ -1,3 +1,17 @@
-# from django.test import TestCase
+from django.test import TestCase
+
+from .models import __str__, full_name
 
 # Create your tests here.
+
+class TestPackage(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_full_name(self):
+        name = 'Patrick Galligan'
+        self.assertEqual(name, full_name('Patrick', 'Galligan'))
+
+    def test___str__(self):
+        pass
