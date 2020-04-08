12aca06759002996b57ae8b1d6593e49edc5b196
Patrick <pgalligan@rockarch.org>
2020-04-08 10:09:00 -0400

    includes model tests
    test coverage for all model methods

--- process_request/tests.py
+++ process_request/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from .models import User
+from .models import MachineUser, User
 
 # Create your tests here.
 
@@ -11,7 +11,18 @@ class TestPackage(TestCase):
 
     def test_full_name(self):
         name = 'Patrick Galligan'
-        self.assertEqual(name, User.full_name('Patrick', 'Galligan'))
+        user = User.objects.create(first_name="Patrick", last_name="Galligan")
+        self.assertEqual(user.full_name, name)
 
-    def test___str__(self):
-        pass
+    def test_user_str(self):
+        full_email = 'Patrick Galligan <pgalligan@rockarch.org>'
+        user = User.objects.create(
+            first_name="Patrick",
+            last_name="Galligan",
+            email="pgalligan@rockarch.org")
+        self.assertEqual(str(user), full_email)
+
+    def test_machineuser_str(self):
+        system = 'Zodiac'
+        user = MachineUser.objects.create(system_name="Zodiac")
+        self.assertEqual(str(user), system)
