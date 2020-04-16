8c8ba9638bbb4f531f6c59fa1dc791c94cfdfb78
Patrick <pgalligan@rockarch.org>
2020-04-16 15:49:58 -0400

    add restrictions
    adds restrictions code

--- process_request/routines.py
+++ process_request/routines.py
@@ -1,4 +1,4 @@
-
+from datetime import datetime
 
 # adding pseudocode
 unsubmitted = []
@@ -21,12 +21,58 @@ class GetRestrictions:
     # 1. PREMIS rights statements first
     # 2. Conditions governing access notes
     # 3. Next closest conditions governing access notes (inherited)
-    pass
-    #def check_restrictions(self, object):
-        #if data_helpers.is_restricted(object):
-            #unsubmitted.append(object.title)
-        #else:
-            #pass
+
+    def is_restricted(object):
+        """Parses an archival object to determine if it is restricted.
+
+        Iterates through notes, looking for a conditions governing access note
+        which contains a particular set of strings.
+        Also looks for associated rights statements which indicate object may be
+        restricted.
+
+        Args:
+            object (JSONModelObject): an ArchivesSpace archival_object.
+
+        Returns:
+            bool: True if archival object is restricted, False if not.
+        """
+        query_string = "materials are restricted"
+        for rights_statement in object.rights_statements:
+            if indicates_restriction(rights_statement):
+                return True
+        for note in object.notes:
+            if note.type == 'accessrestrict':
+                if text_in_note(note, query_string):
+                    return True
+        return False
+
+    def indicates_restriction(rights_statement):
+        """Parses a rights statement to determine if it indicates a restriction.
+
+        Args:
+            rights_statement (JSONModelObject): an ArchivesSpace rights statement.
+
+        Returns:
+            bool: True if rights statement indicates a restriction, False if not.
+        """
+        def is_expired(date):
+            today = datetime.now()
+            date = date if date else datetime.strftime("%Y-%m-%d")
+            return False if (
+                datetime.strptime(date, "%Y-%m-%d") >= today) else True
+
+        rights_json = rights_statement.json()
+        if is_expired(rights_json.get("end_date")):
+            return False
+        for act in rights_json.get("acts"):
+            if (act.get("restriction") in [
+                    "disallow", "conditional"] and not is_expired(act.get("end_date"))):
+                return True
+        return False
+
+    def inherit_restrictions(object):
+        # TO DO: Add code to look up and inherit accessrestrict notes
+        pass
 
 class DeliveryFormats:
 
@@ -57,4 +103,4 @@ class DeliveryFormats:
 
     def return_formats(object):
         for instance in object.instances:
-                pass
+            pass
