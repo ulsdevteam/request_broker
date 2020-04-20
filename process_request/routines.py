0c8ac59e5beedad3e8c562ba96a1b4d1290eb512
Patrick <pgalligan@rockarch.org>
2020-04-20 14:25:23 -0400

    add get note text
    adding method for getting note text

--- process_request/routines.py
+++ process_request/routines.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from rapidfuzz import fuzz
 
 # adding pseudocode
 unsubmitted = []
@@ -25,6 +26,76 @@ class GetRestrictions:
     # 2. Conditions governing access notes
     # 3. Next closest conditions governing access notes (inherited)
 
+    def get_note_text(note):
+        """Parses note content from different note types.
+
+        Args:
+            note (JSONModelObject): an ArchivesSpace note object.
+
+        Returns:
+            list: a list containing note content.
+        """
+        def parse_subnote(subnote):
+            """Parses note content from subnotes.
+
+            Args:
+                subnote (JSONModelObject): an ArchivesSpace subnote object.
+
+            Returns:
+                list: a list containing subnote content.
+            """
+            if subnote.jsonmodel_type in [
+                    'note_orderedlist', 'note_index']:
+                content = subnote.items
+            elif subnote.jsonmodel_type in ['note_chronology', 'note_definedlist']:
+                content = []
+                for k in subnote.items:
+                    for i in k:
+                        content += k.get(i) if isinstance(k.get(i),
+                                                          list) else [k.get(i)]
+            else:
+                content = subnote.content if isinstance(
+                    subnote.content, list) else [subnote.content]
+            return content
+
+        if note.jsonmodel_type == "note_singlepart":
+            content = note.content
+        elif note.jsonmodel_type == 'note_bibliography':
+            data = []
+            data += note.content
+            data += note.items
+            content = data
+        elif note.jsonmodel_type == "note_index":
+            data = []
+            for item in note.items:
+                data.append(item.value)
+            content = data
+        else:
+            subnote_content_list = list(parse_subnote(sn) for sn in note.subnotes)
+            content = [
+                c for subnote_content in subnote_content_list for c in subnote_content]
+        return content
+
+    def text_in_note(note, query_string):
+        """Performs fuzzy searching against note text.
+
+        Args:
+            note (JSONModelObject): an ArchivesSpace note object.
+            query_string (str): a string to match against.
+
+        Returns:
+            bool: True if a match is found for `query_string`, False if no match is
+                found.
+        """
+        CONFIDENCE_RATIO = 97
+        """int: Minimum confidence ratio to match against."""
+        note_content = get_note_text(note)
+        ratio = fuzz.token_sort_ratio(
+            " ".join([n.lower() for n in note_content]),
+            query_string.lower(),
+            score_cutoff=CONFIDENCE_RATIO)
+        return bool(ratio)
+
     def indicates_restriction(rights_statement):
         """Parses a rights statement to determine if it indicates a restriction.
 
@@ -49,24 +120,6 @@ class GetRestrictions:
                 return True
         return False
 
-    def text_in_note(note, query_string):
-        """Performs fuzzy searching against note text.
-
-        Args:
-            note (JSONModelObject): an ArchivesSpace note object.
-            query_string (str): a string to match against.
-
-        Returns:
-            bool: True if a match is found for `query_string`, False if no match is
-                found.
-        """
-        CONFIDENCE_RATIO = 97
-        """int: Minimum confidence ratio to match against."""
-        note_content = get_note_text(note)
-        ratio = fuzz.token_sort_ratio(
-            " ".join([n.lower() for n in note_content]), query_string.lower())
-        return (True if ratio > CONFIDENCE_RATIO else False)
-
     def is_restricted(object):
         """Parses an archival object to determine if it is restricted.
 
@@ -82,13 +135,13 @@ class GetRestrictions:
             bool: True if archival object is restricted, False if not.
         """
         query_string = "materials are restricted"
-        for rights_statement in object.rights_statements:
-            if indicates_restriction(rights_statement):
-                return True
         for note in object.notes:
             if note.type == 'accessrestrict':
                 if text_in_note(note, query_string):
                     return True
+        for rights_statement in object.rights_statements:
+            if indicates_restriction(rights_statement):
+                return True
         return False
 
     def inherit_restrictions(object):
