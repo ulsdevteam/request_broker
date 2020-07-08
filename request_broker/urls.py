675c0643b53bf816f418c2efd4b7be1ee0f29237
Patrick <pgalligan@rockarch.org>
2020-07-08 17:35:32 -0400

    remove empty line
    removing empty line

--- request_broker/urls.py
+++ request_broker/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-
 from process_request.views import ProcessRequestView
 from rest_framework import routers
 
