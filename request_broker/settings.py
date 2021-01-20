ca1e4c1d8dc625cd20b3ee590fd3f95175fc3a35
Hillel Arnold <helrond@hotmail.com>
2021-01-20 08:42:45 -0500

    add cors allowed origins

--- request_broker/settings.py
+++ request_broker/settings.py
@@ -131,7 +131,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # CORS settings
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOWED_ORIGINS = os.environ.get("CORS_ALLOWED_ORIGINS").split(" ")
 
 ARCHIVESSPACE = {
     "baseurl": os.environ.get("AS_BASEURL", "http://sandbox.archivesspace.org:8089/"),
