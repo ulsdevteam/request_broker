2805b1f7bca8ef4d1722cb10a5969f3f80e0eba2
Kevin Cloud <kmc247@pitt.edu>
2021-12-08 14:16:23 -0500

    Debug warning for models

--- request_broker/settings.py
+++ request_broker/settings.py
@@ -109,6 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 AUTH_USER_MODEL = 'process_request.User'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
