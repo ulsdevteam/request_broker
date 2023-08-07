b4d5e1ed18e3f3659575402757f92091ace89e65
Hillel Arnold <hillel.arnold@gmail.com>
2023-08-07 13:15:42 -0400

    updating requirements

--- request_broker/cron.py
+++ request_broker/cron.py
@@ -3,6 +3,7 @@ from django_cron import CronJobBase, Schedule
 from process_request.helpers import refresh_reading_room_cache
 from request_broker.settings import AEON
 
+
 class RefreshReadingRoomCache(CronJobBase):
     schedule = Schedule(run_every_mins=AEON["cache_duration"])
     code = 'request_broker.refresh_reading_room_cache'
