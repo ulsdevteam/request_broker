08551870aaa9c320fb7425b98419921784769997
Hillel Arnold <hillel.arnold@gmail.com>
2023-08-14 13:16:19 -0400

    move collectstatic to entrypoint

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -3,6 +3,10 @@
 ./wait-for-it.sh db:${SQL_PORT} -- echo "Apply database migrations"
 python manage.py migrate
 
+# Collect static files
+echo "Collecting static files"
+python manage.py collectstatic
+
 #Start cron
 echo "Starting cron"
 cron
